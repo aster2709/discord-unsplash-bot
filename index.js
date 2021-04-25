a89ae132a8a56c5584c82eed7e0feb47972696fc
yash <ymehta777@gmail.com>
2021-04-25 10:36:18 +0530

    Fetch a random image with entered keyword and successfully send the message in discord

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const { createApi } = require("unsplash-js");
+const fetch = require("node-fetch");
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const { accessKey, prefix, token } = require("./config.json");
+global.fetch = fetch;
 const unsplash = createApi({
   accessKey: accessKey,
 });
-
+client.once("ready", () => {
+  console.log("Ready");
+});
+client.on("message", (msg) => {
+  if (msg.content.startsWith(`${prefix}`)) {
+    const keyword = msg.content.match(/[a-zA-Z]+/);
+    unsplash.photos
+      .getRandom({
+        query: keyword,
+      })
+      .then((res) => {
+        if (res.errors) {
+          console.log(`error occured for keyword ${keyword}: ${res.errors[0]}`);
+          msg.channel.send(
+            `Ran into a problem, please try with another keyword`
+          );
+        } else {
+          const img = res.response;
+          msg.channel.send(
+            new Discord.MessageEmbed()
+              .setColor("#0099ff")
+              .setTitle(`Photo by ${img.user.name} on Unsplash`)
+              .setURL(`${img.links.download}`)
+              .setImage(img.urls.thumb)
+          );
+        }
+      });
+  }
+});
 client.login(token);
